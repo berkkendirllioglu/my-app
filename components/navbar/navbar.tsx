21fafec25cd20043d682bed0da85dfed6b770314
berkkendirlioglu <berkkplayer@hotmail.com>
2025-03-05 16:04:12 +0300

    changes

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className='flex w-full py-4 px-20 justify-around items-center bg-[#333]'>
       <Link className='text-[#fff] transition hover:text-[#999999]' href={"/"}>Home</Link>
       <Link className='text-[#fff] transition hover:text-[#999999]' href={"/todos"}>Todos</Link>
-      <Link className='text-[#fff] transition hover:text-[#999999]' href={"/todos/1"}>Single Todo</Link>
+      <Link className='text-[#fff] transition hover:text-[#999999]' href={"/todos/3"}>Single Todo</Link>
     </nav>
   )
 }
