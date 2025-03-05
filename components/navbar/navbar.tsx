8807ab8b3af9f14e28a959456a0d2d660e4e4d96
berk <berkkendirlioglu@elmaligroup.com>
2025-03-05 15:53:50 +0300

    place-changes

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='flex w-full py-2 px-20 justify-between items-center bg-[#333]'>
+    <nav className='flex w-full py-2 px-20 justify-around items-center bg-[#333]'>
       <Link className='text-[#fff]' href={"/"}>Home</Link>
       <Link className='text-[#fff]' href={"/todos"}>Todos</Link>
       <Link className='text-[#fff]' href={"/todos/1"}>Single Todo</Link>
